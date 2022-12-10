4eeb28a7676e636c47fe7b1f826b9051997e7aed
ハトネコエ hatonekoe@gmail.com <hato.nekoe@gmail.com>
2022-12-10 13:12:18 +0900

    Add version option

--- cmd/params.go
+++ cmd/params.go
@@ -2,7 +2,9 @@ package cmd
 
 import (
 	"flag"
+	"fmt"
 	"log"
+	"os"
 )
 
 type parameters struct {
@@ -15,9 +17,11 @@ type parameters struct {
 	FetchPullRequestsLimit int
 	EnterpriseURL          string
 	DryRun                 bool
+	ShowVersion            bool
 }
 
 var params parameters
+var Version = "v1.7.0"
 
 func ParseParameters() {
 	flag.StringVar(&params.GitHubAPIToken, "token", "", "[Required] GitHub API Token")
@@ -31,8 +35,14 @@ func ParseParameters() {
 	flag.IntVar(&params.FetchPullRequestsLimit, "limit", FetchPullRequestsLimitDefault, "[Opiton] limit number of fetching pull requests")
 	flag.StringVar(&params.EnterpriseURL, "enterprise-url", "", "[Opiton] URL of GitHub Enterprise (ex. https://github.your.domain )")
 	flag.BoolVar(&params.DryRun, "dry-run", false, "[Opiton] Only display, not create or update a pull request")
+	flag.BoolVar(&params.ShowVersion, "version", false, "[Opiton] Show version")
+	flag.BoolVar(&params.ShowVersion, "v", false, "[Opiton] Shorthand of -version")
 	flag.Parse()
 
+	if params.ShowVersion {
+		fmt.Println(Version)
+		os.Exit(0)
+	}
 	if params.GitHubAPIToken == "" {
 		log.Fatalln("-token is required")
 	}
